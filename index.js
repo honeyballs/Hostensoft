8db1334652d0ba6e6f6e25527018a29d18c87286
Unknown <yannick.bals@web.de>
2018-12-28 18:13:34 +0100

    Initialize application

---
+++ index.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let path = require('path');
+
+let app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', '/index.html'));
+})
+
+app.listen(3000, () => console.log("Listening on port 3000"));
