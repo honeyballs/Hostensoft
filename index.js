34c0c6290e8a2bc2745237216ed44d0ebf56457a
Unknown <yannick.bals@web.de>
2018-12-29 15:45:05 +0100

    Add files, write html and styles

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let path = require('path');
 
 let app = express();
 
+app.use(express.static(__dirname + "/public"));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public', '/index.html'));
 })
