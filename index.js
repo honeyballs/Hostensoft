d09f723b06741cda99e626a54a9ea967e24bbc14
Unknown <yannick.bals@web.de>
2018-12-31 15:15:05 +0100

    Get the file tree via drag & drop

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public', '/index.html'));
 })
 
+app.post('/upload', (req, res) => {
+    console.log(req);
+})
+
 app.listen(3000, () => console.log("Listening on port 3000"));
--- public/main.js
+++ public/main.js
@@ -1,4 +1,8 @@
 let droparea = document.getElementById('drag-box');
+let inputfiles = document.getElementById('file-box');
+
+// The form which will be uploaded to the server
+var formData = new FormData();
 
 // Prevent all default events
 ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
@@ -18,6 +22,7 @@ let droparea = document.getElementById('drag-box');
 
 // Handle the actual upload
 droparea.addEventListener('drop', handleDrop, false);
+inputfiles.addEventListener('change', handlePick, false);
 
 /* Event handlers */
 
@@ -34,15 +39,75 @@ function unhighlightArea(e) {
     droparea.classList.remove('highlight');
 }
 
+// Put files into the formdata
 function handleDrop(e) {
     let dt = e.dataTransfer;
-    let files = dt.files;
-    console.log(dt);
-    console.log('++++++++++++++++++++++++++++');
-    console.log(files);
-    handleFiles(files);
+    let items = dt.items;
+
+    if (items.length === 1) {
+        let entry = items[0].webkitGetAsEntry();
+
+        // Only allow to upload directories
+        if (entry.isDirectory) {
+            let projectName = entry.name;
+
+            formData.append("project", projectName);
+
+            let dirReader = entry.createReader();
+            
+            // Run recursively through the file tree.
+            // Only continue when all files are available. (All promises resolved) 
+            dirReader.readEntries(entries => {
+                var promises = [];
+                for (var i = 0; i < entries.length; i++) {
+                    promises.push(traverseTree(entries[i], projectName + "/", formData));
+                }
+                Promise.all(promises).then(values => {
+                    // Display the key/value pairs
+                    for (var pair of formData.entries()) {
+                        console.log(pair[0] + ', ' + pair[1]);
+                    }
+                });
+            })
+
+        } else {
+            alert("Please upload a directory.");
+        }
+    } else {
+        alert("Please upload only one folder.");
+    }
+
+}
+
+// Recursive function to look through directories/files
+function traverseTree(item, path, form) {
+    path = path || "";
+    return new Promise((resolve, reject) => {
+        if (item.isFile) {
+            // Get the file
+            item.file(file => {
+                //console.log("File: ", path + file.name);
+                form.append(path, file);
+                resolve(true);
+            })
+        } else if (item.isDirectory) {
+            let dirReader = item.createReader();
+            dirReader.readEntries(entries => {
+                var promises = [];
+                for (var i = 0; i < entries.length; i++) {
+                    promises.push(traverseTree(entries[i], path + item.name + "/", form));
+                }
+                Promise.all(promises).then(values => resolve(true));
+            })
+        }
+    })
+
+}
+
+function handlePick(e) {
+    handleFiles(e.target.files)
 }
 
 function handleFiles(files) {
-    
+    console.log(files)
 }
